5fcfeb1ce6b86e3e1aec275fbb1689a477452aff
Tyler Le <tylerle99@yahoo.com>
2023-04-01 22:31:46 -0700

    add refresh prompt upon first-time installation

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    checkFirstRun();
     chrome.storage.sync.get(['distance'], function (result) {
         const pxContainer = document.getElementById("px-dist");
         const ftContainer = document.getElementById("ft-dist");
@@ -23,4 +24,18 @@ function renderPopup(pxContainer, ftContainer) {
         ftContainer.textContent = `${pixelsToFeet}`;
     });
 
+}
+
+function checkFirstRun() {
+    const firstRun = localStorage.getItem("firstRun") === null;
+    if (firstRun) {
+        const main = document.querySelector("main");
+
+        // Display a message to the user to restart their browser
+        main.innerHTML = `<div style="border: 1px solid; margin: 10px 0px; padding: 15px; color: #D8000C; background-color: #FFBABA;">Please restart your browser to complete the installation of this extension.</div>`;
+
+        // Set a flag in local storage to indicate that the extension has already been run
+        localStorage.setItem("firstRun", "false");
+        return;
+    }
 }
